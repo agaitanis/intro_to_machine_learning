6928cdb326ccd87f2077bde87c932dd7bafdf8d6
Alexandros Gaitanis <alexandros.gaitanis@gmail.com>
2018-09-12 19:58:09 +0300

    Enron data exploration

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,22 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print 'Number of people =', len(enron_data)
+print 'Number of features =', len(enron_data.itervalues().next())
+
+pois = 0
+for name, data in enron_data.iteritems():
+    pois += data['poi']
+print 'Number of POIs =', pois
+
+f = open('../final_project/poi_names.txt')
+text = f.read()
+print 'Total number of POIs =', text.count('(n)') + text.count('(y)')
+f.close()
+
+print 'Total stock value belonging to James Prentice =', enron_data['PRENTICE JAMES']['total_stock_value']
+
+print 'Number of messages from Wesley Colwell =', enron_data['COLWELL WESLEY']['from_this_person_to_poi']
+
+print 'Value of stock options exercised by Jeffrey K Skilling =', enron_data['SKILLING JEFFREY K']['exercised_stock_options']
 
